82867a16e9a1bf623e300110c0eadb40441b283c
Dudododo <a752601260@gmail.com>
2020-03-27 14:25:22 +0800

    添加项目文件。

---
+++ average_filter2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	VideoCapture cap;
+
+	cap.open(0);
+
+	while (1)
+	{
+		cv::Mat src;
+		cv::Mat dst;
+
+		cap >> src;
+		cv::GaussianBlur(src, dst, Size(7, 7), 1); //均值滤波
+
+		cv::imshow("原图", src);
+		cv::imshow("均值滤波", dst);
+
+		waitKey(30);
+	}
+}
